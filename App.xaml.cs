e2772dcb3cf555dae229b9e673bec050d3273474
Vitor Burns <122344541+vitorburns@users.noreply.github.com>
2025-03-23 20:16:23 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Space_battle_shooter_WPF_MOO_ICT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
